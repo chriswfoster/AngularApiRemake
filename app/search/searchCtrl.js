5b69a7bc1d0111f3800803ce6ec9ea8c6c9286b0
Chris Foster <chriswfoster@gmail.com>
2017-12-07 19:23:42 -0600

    I dono

--- app/search/searchCtrl.js
+++ app/search/searchCtrl.js
@@ -2,9 +2,12 @@ angular
   .module("giphyApp")
   .controller("searchCtrl", function($scope, $stateParams, mainSrvc) {
       console.log(text)
+
     mainSrvc.getImages(textz).then(function(response) {
       $scope.allImages = response.data
     })
-
+$scope.whatever = function(){
+    console.log("hi mom")
+}
     $scope.text = {}
   })
---
+++ app/search/searcher.js
@@ -0,0 +1,8 @@
+angular.module("giphyApp").component("searcher", {
+    templateUrl: "app/search/searchTmpl.html",
+    controller: function whatever() {
+      console.log('hi mom')
+     
+    },
+    controllerAs: "tdv"
+})
